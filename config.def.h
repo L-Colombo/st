8baf568a66af1f6de6492568329105c3a6f72302
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-01-13 14:17:03 +0100

    change font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IntoneMono Nerd Font Propo:size=14";
+static char *font = "Iosevka Nerd Font Propo:bold:size=15";
 static int borderpx = 8;
 
 /*
