f93f726809c4eae290544935b81f919bc01d8ef4
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-03-05 09:52:26 +0100

    change font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font Propo:size=15";
+static char *font = "JetBrains Mono NL:size=14";
 static int borderpx = 8;
 
 /*
